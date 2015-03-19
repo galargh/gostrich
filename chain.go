9295ad5d96e3d17df65394844dff4c5842a48249
Piotr Galar <fjalar@ip-192-168-1-71.tkb.int>
2015-03-19 23:00:43 +0100

    First commit

---
+++ chain.go
@@ -0,0 +1,147 @@
+// Copyright 2015 Piotr Galar. All rights reserved.
+// Based on the path package, Copyright 2009 The Go Authors.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
+// Package gost provides a simple function chaining mechanism.
+// It allows chaining functions and the arguments they require.
+// It does not ensure the correctness of a chain until the build is requested.
+package gost
+
+import (
+	"reflect"
+)
+
+// Chain holds a current list of links to chain.
+type Chain struct {
+	links []interface{}
+}
+
+// VarArgs is a named type used to define the number of arguments
+// that should be passed to the variadic parameter of the preceeding function.
+type VarArgs int
+
+// New creates a new empty chain where links are set to nil.
+func New() *Chain {
+	return &Chain{nil}
+}
+
+// Compose is a method which appends links passed to the current chain.
+// ie.
+// Chain([f, g]).Compose(h, k) -> Chain([f, g, h, k]); f(g(h(k)))
+func (c *Chain) Compose(links ...interface{}) *Chain {
+	return &Chain{append(c.links, links...)}
+}
+
+// Then is a method which prepends links passed to the current chain.
+// It uses the reverse order.
+// ie.
+// Chain([f, g]).Then(h, k) -> Chain([k, h, f, g]); k(h(f(g)))
+func (c *Chain) Then(links ...interface{}) *Chain {
+	var reverse []interface{}
+	if n := len(links); n > 0 {
+		reverse = make([]interface{}, n)
+		for i, link := range links {
+			reverse[n - 1 - i] = link
+		}
+	}
+	return &Chain{append(reverse, c.links...)}
+}
+
+// MergeCompose is a method which appends links from the chains passed
+// to the current chain.
+// ie.
+// Chain([f, g]).MergeCompose(Chain([h, k]), Chain([l, m]))
+// -> Chain([f, g, h, k, l, m]); f(g(h(k(l(m)))))
+func (c *Chain) MergeCompose(chains ...*Chain) *Chain {
+	result := &Chain{c.links}
+	for _, chain := range chains {
+		result = result.Compose(chain.links...)
+	}
+	return result
+}
+
+// MergeThen is a method which prepends links from the chains passed
+// to the current chain in the revese order.
+// ie.
+// Chain([f, g]).MergeThen(Chain([h, k]), Chain([l, m]))
+// -> Chain([k, h, m, l, f, g]); k(h(m(l(f(g)))))
+func (c *Chain) MergeThen(chains ...*Chain) *Chain {
+	result := &Chain{c.links}
+	n := len(chains)
+	reverse := make([]*Chain, n)
+	for i, chain := range chains {
+		reverse[n - 1 - i] = chain
+	}
+	for _, chain := range chains {
+		result = result.Then(chain.links...)
+	}
+	return result
+}
+
+// Build is a method which executes the constructed chain and
+// returns an array of interface{} containing the return values.
+//
+// eg.
+// plus: f(int, int) -> int
+// timesTwo: f(int) -> int
+// n: int = 2
+// m: int = 3
+// chain: Chain([f, timesTwo, n, m])
+// chain.Build() returns [f(timesTwo(n), m)] = [7]
+//
+// It traverses the chain in the reverse order, puts values on the stack,
+// executes functions with the correct number of arguments taken from the stack,
+// puts return values on the stack in the reverse order.
+//
+// For functions with a variadic parameter it:
+// -if top(stack) is VarArgs: takes VarArgs number of arguments for the variadic parameter
+// -if top(stack) is not VarArgs: takes as many arguments as possible, depending on the type
+//
+// It panics when there are not enough values on the stack for the current function call.
+//
+// Be careful about the references as links inside the chain.
+func (c *Chain) Build() []interface{} {
+	var stack []reflect.Value
+	for i := len(c.links) - 1; i >= 0; i-- {
+		value := reflect.ValueOf(c.links[i])
+		switch value.Kind() {
+		case reflect.Func:
+			n, m := len(stack), value.Type().NumIn()
+			if (value.Type().IsVariadic() && n != 0) {
+				if variadicNo, ok := stack[n-1].Interface().(VarArgs); ok {
+					m, n = m + int(variadicNo) - 1, n - 1
+					stack = stack[:n]
+				} else {
+					variadicType := value.Type().In(m - 1).Elem()
+					for n - m >= 0 && stack[n-m].Type() == variadicType {
+						m++
+					}
+					m--
+				}
+			}
+			if (n - m < 0) {
+				panic("Gost: Build with incomplete chain")
+			}
+			args := make([]reflect.Value, m)
+			for j, arg := range stack[n-m:] {
+				args[m - 1 - j] = arg
+			}
+			rets := value.Call(args)
+			p := len(rets)
+			reverseRets := make([]reflect.Value, p)
+			for j, ret := range rets {
+				reverseRets[p - 1 - j] = ret
+			}
+			stack = append(stack[:n-m], reverseRets...)
+		default:
+			stack = append(stack, value)
+		}
+	}
+	n := len(stack)
+	result := make([]interface{}, n)
+	for i, value := range(stack) {
+		result[n - 1 - i] = value.Interface()
+	}
+	return result
+}
---
+++ chain_test.go
@@ -0,0 +1,185 @@
+package gost
+
+import (
+	"testing"
+)
+
+func TestBuildOnNewChainCreatesEmptyArray(t *testing.T) {
+	result := New().Build()
+	if (result == nil || len(result) != 0) {
+		t.Fail()
+	}
+}
+
+func TestNewCreatesNewChain(t *testing.T) {
+	chain := New()
+	otherChain := New()
+	if (&chain == &otherChain || &chain.links == &otherChain.links || chain.links != nil || otherChain.links != nil) {
+		t.Fail()
+	}
+}
+
+func TestComposeCreatesNewChain(t *testing.T) {
+	chain := New()
+	composed := chain.Compose(1)
+	if (&chain == &composed || &chain.links == &composed.links || chain.links != nil || composed.links == nil) {
+		t.Fail()
+	}
+}
+
+func TestThenCreatesNewChain(t *testing.T) {
+	chain := New()
+	then := chain.Then(1)
+	if (&chain == &then || &chain.links == &then.links || chain.links != nil || then.links == nil) {
+		t.Fail()
+	}
+}
+
+func TestMergeComposeCreatesNewChain(t *testing.T) {
+	chain := New()
+	merged := chain.MergeCompose(New())
+	if (&chain == &merged || &chain.links == &merged.links || chain.links != nil || merged.links != nil) {
+		t.Fail()
+	}
+}
+
+func TestMergeThenCreatesNewChain(t *testing.T) {
+	chain := New()
+	merged := chain.MergeThen(New())
+	if (&chain == &merged || &chain.links == &merged.links || chain.links != nil || merged.links != nil) {
+		t.Fail()
+	}
+}
+
+func TestComposeCreatesChainWithLinksInTheGivenOrder(t *testing.T) {
+	chain := New().Compose(1, 2)
+	if (chain.links[0] != 1 || chain.links[1] != 2) {
+		t.Fail()
+	}
+}
+
+
+func TestThenCreatesChainWithLinksInTheReverseOrder(t *testing.T) {
+	chain := New().Then(1, 2)
+	if (chain.links[0] != 2 || chain.links[1] != 1) {
+		t.Fail()
+	}
+}
+
+func TestMergeComposeCreatesChainWithLinksFromChainsInTheGivenOrder(t *testing.T) {
+	chain1 := New().Compose(1, 2)
+	chain2 := New().Compose(3, 4)
+	chain := New().Compose(0).MergeCompose(chain1, chain2)
+	if (chain.links[0] != 0 || chain.links[1] != 1 || chain.links[2] != 2 || chain.links[3] != 3 || chain.links[4] != 4) {
+		t.Fail()
+	}
+}
+
+func TestMergeThenCreatesChainWithLinksFromChainsInTheReverseOrder(t *testing.T) {
+	chain1 := New().Compose(1, 2)
+	chain2 := New().Compose(3, 4)
+	chain := New().Compose(0).MergeThen(chain1, chain2)
+	if (chain.links[0] != 4 || chain.links[1] != 3 || chain.links[2] != 2 || chain.links[3] != 1 || chain.links[4] != 0) {
+		t.Fail()
+	}
+}
+
+func TestBuildWithVariadicFunctionAndNoVarArgsTakesAsManyArgsAsPossible(t *testing.T) {
+	varFunc := func(a int, ns ...int) int {
+		result := 0
+		for _, n := range ns {
+			result += a * n
+		}
+		return result
+	}
+	simpleFunc := func() int {
+		return 3
+	}
+	result := New().Compose(varFunc).Compose(10, 1, 2, simpleFunc, "dog", 9).Build()
+	if (len(result) != 3 || result[0] != 60 || result[1] != "dog" || result[2] != 9) {
+		t.Fail()
+	}
+}
+
+func TestBuildWithVariadicFunctionAndVarArgsSetTakesLimitedNumberOfArgs(t *testing.T) {
+	varFunc := func(a int, ns ...int) int {
+		result := 0
+		for _, n := range ns {
+			result += a * n
+		}
+		return result
+	}
+	simpleFunc := func() int {
+		return 3
+	}
+	result := New().Compose(varFunc, VarArgs(2)).Compose(10, 1, 2, simpleFunc, "dog", 9).Build()
+	if (len(result) != 4 || result[0] != 30 || result[1] != 3 || result[2] != "dog" || result[3] != 9) {
+		t.Fail()
+	}
+}
+
+func TestBuildShouldExecuteInTheLeftToRightManner(t *testing.T) {
+	f1 := func(a int, b int, c int, d int) (int, int, int, int) {
+		return a, b, c, d
+	} 
+	f2 := func(a int, b int) int {
+		return a + b
+	}
+	f3 := func(a int, b int, c int) (int, int) {
+		return a * b, b * c
+	}
+	result := New().Compose(f1, f2, 10, 5, f3, 2, 4, 8, 100).Build()
+	if (len(result) != 4 || result[0] != 15 || result[1] != 8 || result[2] != 32 || result[3] != 100) {
+		t.Fail()
+	}
+}
+
+func TestComposeAcceptsNoArguments(t *testing.T) {
+	chain := New()
+	composed := chain.Compose()
+	if (&chain == &composed || &chain.links == &composed.links || chain.links != nil || composed.links != nil) {
+		t.Fail()
+	}
+}
+
+func TestThenAcceptsNoArguments(t *testing.T) {
+	chain := New()
+	then := chain.Compose()
+	if (&chain == &then || &chain.links == &then.links || chain.links != nil || then.links != nil) {
+		t.Fail()
+	}
+}
+
+func TestMergeComposeAcceptsNoArgumentsAndReturnChainCopy(t *testing.T) {
+	chain := New()
+	composed := chain.MergeCompose()
+	if (&chain == &composed || &chain.links == &composed.links || chain.links != nil || composed.links != nil) {
+		t.Fail()
+	}
+}
+
+func TestMergeThenAcceptsNoArgumentsAndReturnsChainCopy(t *testing.T) {
+	chain := New()
+	then := chain.MergeThen()
+	if (&chain == &then || &chain.links == &then.links || chain.links != nil || then.links != nil) {
+		t.Fail()
+	}
+}
+
+func TestComposeAcceptsNil(t *testing.T) {
+	chain := New()
+	composed := chain.Compose(nil)
+	if (&chain == &composed || &chain.links == &composed.links || chain.links != nil || composed.links == nil ||
+		composed.links[0] != nil) {
+		t.Fail()
+	}
+}
+
+func TestThenAcceptsNil(t *testing.T) {
+	chain := New()
+	then := chain.Compose(nil)
+	if (&chain == &then || &chain.links == &then.links || chain.links != nil || then.links == nil ||
+		then.links[0] != nil) {
+		t.Fail()
+	}
+}
